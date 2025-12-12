70928ca4e00e6140ef157d1a402f7bc097f73075
HAN IN DEOK <167001808+deokduckhan@users.noreply.github.com>
2025-12-12 23:42:08 +0900

    Delete CalendarSwing directory

--- CalendarSwing/src/CalendarSwing_Repo/finance-server/src/main/java/com/example/finance_server/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.finance_server; 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
